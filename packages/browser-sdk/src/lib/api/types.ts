29728d88968593106498eb1ee6408f562682068b
James Dooley <james.dooley@whereby.com>
2024-02-13 16:53:27 +0800

    remove api from browser-sdk

--- packages/browser-sdk/src/lib/api/types.ts
+++
@@ -1,5 +0,0 @@
-export type UserConsentAction = "accepted" | "rejected" | null;
-export type ConsentGrantRequest = {
-    readonly consentRevisionId: string;
-    readonly action: UserConsentAction;
-};
