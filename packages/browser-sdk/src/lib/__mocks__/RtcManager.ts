364724c1f020de6f64c2059f4a5025feb4f33cc0
James Dooley <james.dooley@whereby.com>
2024-02-13 16:54:25 +0800

    Remove mocks

--- packages/browser-sdk/src/lib/__mocks__/RtcManager.ts
+++
@@ -1,6 +0,0 @@
-import RtcManager from "@whereby/jslib-media/src/webrtc/RtcManager";
-
-export default jest.fn(function (this: RtcManager): RtcManager {
-    this.acceptNewStream = jest.fn();
-    return this;
-});
