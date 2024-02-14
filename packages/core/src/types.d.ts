41219e4708f6391f24e0e6841b6b4182113c2ad7
James Dooley <james.dooley@whereby.com>
2024-02-14 12:38:07 +0800

    WIP: parity with main

--- packages/core/src/types.d.ts
+++ packages/core/src/types.d.ts
@@ -1,24 +1,3 @@
-// declare module "heresy" {
-//     interface AttrChanged {
-//         attributeName: string;
-//         oldValue: string | boolean;
-//     }
-
-//     interface element {
-//         observedAttributes?: string[];
-//         onattributechanged?: (attrChanged: AttrChanged) => void;
-//         onconnected?: () => void;
-//         ondisconnected?: () => void;
-//         oninit?: () => void;
-//         render: () => void;
-//         // eslint-disable-next-line @typescript-eslint/no-explicit-any
-//         [x: string]: any;
-//     }
-
-//     export function define(elementName: string, element: element): void;
-//     export function ref(): boolean;
-// }
-
 declare module "@whereby/jslib-media/src/webrtc/RtcManagerDispatcher" {
     enum RtcEventNames {
         rtc_manager_created = "rtc_manager_created",
