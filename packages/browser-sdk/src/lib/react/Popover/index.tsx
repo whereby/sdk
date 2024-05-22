cfaf6f61309d0fd15acd81e16310796966dd959d
Thomas Frivold <thomasdfrivold@gmail.com>
2024-05-22 15:41:38 +0200

    browser-sdk: Add popover component

---
+++ packages/browser-sdk/src/lib/react/Popover/index.tsx
@@ -0,0 +1,27 @@
+import * as React from "react";
+
+import "./styles.css";
+import * as PopoverPrimitive from "@radix-ui/react-popover";
+import cn from "clsx";
+
+const Popover = PopoverPrimitive.Root;
+
+const PopoverTrigger = PopoverPrimitive.Trigger;
+
+const PopoverContent = React.forwardRef<
+    React.ElementRef<typeof PopoverPrimitive.Content>,
+    React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
+>(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+    <PopoverPrimitive.Portal>
+        <PopoverPrimitive.Content
+            ref={ref}
+            align={align}
+            sideOffset={sideOffset}
+            className={cn("popoverContent", className)}
+            {...props}
+        />
+    </PopoverPrimitive.Portal>
+));
+PopoverContent.displayName = PopoverPrimitive.Content.displayName;
+
+export { Popover, PopoverTrigger, PopoverContent };
