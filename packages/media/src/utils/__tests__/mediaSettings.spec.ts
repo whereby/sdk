3e1acc380c6006ea20671579076f60d5961f09b0
Kevin Hanna <kevin.hanna@whereby.com>
2025-03-20 15:59:30 +0000

    media: Sort codecs by power efficiency
    media: add preferHardwareDecodingOn flag
    
    allows us to sort video codecs based on hardware decodability

--- packages/media/src/utils/__tests__/mediaSettings.spec.ts
+++ packages/media/src/utils/__tests__/mediaSettings.spec.ts
@@ -1,7 +1,7 @@
-import { sortCodecsByMimeType } from "../mediaSettings";
+import { sortCodecs } from "../mediaSettings";
 import { type Codec } from "../mediaSettings";
 
-describe("sortCodecsByMimeType", () => {
+describe("sortCodecs", () => {
     const codecs: Codec[] = [
         {
             clockRate: 90000,
@@ -34,50 +34,42 @@ describe("sortCodecsByMimeType", () => {
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42e01f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42e01f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d001f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=4d001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=4d001f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=f4001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=f4001f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=f4001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=f4001f",
         },
         {
             clockRate: 90000,
@@ -92,14 +84,12 @@ describe("sortCodecsByMimeType", () => {
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=64001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=64001f",
         },
         {
             clockRate: 90000,
             mimeType: "video/H264",
-            sdpFmtpLine:
-                "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=64001f",
+            sdpFmtpLine: "level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=64001f",
         },
         {
             clockRate: 90000,
@@ -116,55 +106,110 @@ describe("sortCodecsByMimeType", () => {
         },
     ];
     describe("with no flags enabled", () => {
-        it("returns the array unchanged", () => {
-            expect(sortCodecsByMimeType(codecs, {})).toEqual(codecs);
+        it("returns the array unchanged", async () => {
+            const sorted = await sortCodecs(codecs, {});
+
+            expect(sorted).toEqual(codecs);
         });
     });
 
     describe("with vp9On enabled", () => {
-        it("returns the sorted array", () => {
-            const sorted = sortCodecsByMimeType(codecs, { vp9On: true });
-            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/vp9/i)))
-                .toEqual(0);
+        it("returns the sorted array", async () => {
+            const sorted = await sortCodecs(codecs, { vp9On: true });
+
+            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/vp9/i))).toEqual(0);
         });
 
-        it("leaves the rest of the array in order", () => {
-            const removeVp9 = (codec: Codec) => !codec.mimeType.match(/vp9/i)
-            const sortedFiltered = sortCodecsByMimeType(codecs, { av1On: true, vp9On: true }).filter(removeVp9);
-            const unsortedFiltered = codecs.filter(removeVp9)
-            expect(sortedFiltered).toEqual(unsortedFiltered)
-        })
+        it("leaves the rest of the array in order", async () => {
+            const removeVp9 = (codec: Codec) => !codec.mimeType.match(/vp9/i);
+            const sortedFiltered = (await sortCodecs(codecs, { av1On: true, vp9On: true })).filter(removeVp9);
+            const unsortedFiltered = codecs.filter(removeVp9);
+
+            expect(sortedFiltered).toEqual(unsortedFiltered);
+        });
     });
 
     describe("with av1On enabled", () => {
-        it("returns the sorted array", () => {
-            const sorted = sortCodecsByMimeType(codecs, { av1On: true });
-            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/av1/i)))
-                .toEqual(0);
+        it("returns the sorted array", async () => {
+            const sorted = await sortCodecs(codecs, { av1On: true });
+
+            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/av1/i))).toEqual(0);
         });
 
-        it("leaves the rest of the array in order", () => {
-            const removeAv1 = (codec: Codec) => !codec.mimeType.match(/av1/i)
-            const sortedFiltered = sortCodecsByMimeType(codecs, { av1On: true, vp9On: true }).filter(removeAv1);
-            const unsortedFiltered = codecs.filter(removeAv1)
-            expect(sortedFiltered).toEqual(unsortedFiltered)
-        })
+        it("leaves the rest of the array in order", async () => {
+            const removeAv1 = (codec: Codec) => !codec.mimeType.match(/av1/i);
+            const sortedFiltered = (await sortCodecs(codecs, { av1On: true, vp9On: false })).filter(removeAv1);
+            const unsortedFiltered = codecs.filter(removeAv1);
+
+            expect(sortedFiltered).toEqual(unsortedFiltered);
+        });
     });
 
     describe("with av1On and vp9On enabled", () => {
-        it("prioritises AV1", () => {
-            const sorted = sortCodecsByMimeType(codecs, { av1On: true, vp9On: true });
-            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/av1/i)))
-                .toEqual(0);
-            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/vp9/i)))
-                .toEqual(2);
+        it("prioritises AV1", async () => {
+            const sorted = await sortCodecs(codecs, { av1On: true, vp9On: true });
+
+            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/av1/i))).toEqual(0);
+            expect(sorted.findIndex((codec: Codec) => !!codec.mimeType.match(/vp9/i))).toEqual(2);
         });
 
-        it("leaves the rest of the array in order", () => {
-            const removeVp9AndAv1 = (codec: Codec) => !codec.mimeType.match(/vp9|av1/i)
-            const sortedFiltered = sortCodecsByMimeType(codecs, { av1On: true, vp9On: true }).filter(removeVp9AndAv1);
-            const unsortedFiltered = codecs.filter(removeVp9AndAv1)
-            expect(sortedFiltered).toEqual(unsortedFiltered)
-        })
-    })
+        it("leaves the rest of the array in order", async () => {
+            const removeVp9AndAv1 = (codec: Codec) => !codec.mimeType.match(/vp9|av1/i);
+            const sortedFiltered = (await sortCodecs(codecs, { av1On: true, vp9On: true })).filter(removeVp9AndAv1);
+            const unsortedFiltered = codecs.filter(removeVp9AndAv1);
+
+            expect(sortedFiltered).toEqual(unsortedFiltered);
+        });
+    });
+
+    describe("with preferHardwareDecodingOn enabled", () => {
+        it("puts hardware decodable codecs first", async () => {
+            const codecs = [
+                {
+                    clockRate: 90000,
+                    mimeType: "video/VP8",
+                },
+                {
+                    clockRate: 90000,
+                    mimeType: "video/VP9",
+                },
+                {
+                    clockRate: 90000,
+                    mimeType: "video/AV1",
+                },
+                {
+                    clockRate: 90000,
+                    mimeType: "video/H264",
+                },
+            ];
+            const decodingInfo = jest.fn();
+            decodingInfo.mockImplementation((config: { video: { contentType: string } }) => {
+                return {
+                    powerEfficient: config.video.contentType !== "video/VP8",
+                };
+            });
+            Object.assign(globalThis.navigator, { mediaCapabilities: { decodingInfo } });
+
+            const sorted = await sortCodecs(codecs, { preferHardwareDecodingOn: true });
+
+            expect(sorted).toEqual([
+                {
+                    clockRate: 90000,
+                    mimeType: "video/VP9",
+                },
+                {
+                    clockRate: 90000,
+                    mimeType: "video/AV1",
+                },
+                {
+                    clockRate: 90000,
+                    mimeType: "video/H264",
+                },
+                {
+                    clockRate: 90000,
+                    mimeType: "video/VP8",
+                },
+            ]);
+        });
+    });
 });
--- packages/media/src/webrtc/P2pRtcManager.ts
+++ packages/media/src/webrtc/P2pRtcManager.ts
@@ -15,7 +15,7 @@ import validate from "uuid-validate";
 import rtcManagerEvents from "./rtcManagerEvents";
 import Logger from "../utils/Logger";
 import { CustomMediaStreamTrack, RtcManager } from "./types";
-import { sortCodecsByMimeType } from "../utils";
+import { sortCodecs } from "../utils";
 import { maybeTurnOnly, external_stun_servers, turnServerOverride } from "../utils/iceServers";
 
 // @ts-ignore
@@ -922,18 +922,11 @@ export default class P2pRtcManager implements RtcManager {
         }
     }
 
-    _setCodecPreferences(
-        pc: RTCPeerConnection,
-        {
-            p2pVp9On,
-            p2pAv1On,
-            redOn,
-        }: {
-            p2pVp9On?: boolean;
-            p2pAv1On?: boolean;
-            redOn?: boolean;
-        },
-    ) {
+    async _setCodecPreferences(pc: RTCPeerConnection) {
+        const { p2pVp9On, p2pAv1On, redOn, preferHardwareDecodingOn } = this._features;
+        if (!(p2pVp9On || p2pAv1On || redOn || preferHardwareDecodingOn)) {
+            return;
+        }
         try {
             // audio
             const audioTransceivers = pc
@@ -957,23 +950,26 @@ export default class P2pRtcManager implements RtcManager {
             // video
             const videoTransceivers = pc
                 .getTransceivers()
-                .filter((transceiver: any) => transceiver?.sender?.track?.kind === "video");
+                .filter((transceiver) => transceiver?.sender?.track?.kind === "video");
 
-            videoTransceivers.forEach((videoTransceiver: any) => {
-                // If not implemented return
-                if (RTCRtpReceiver.getCapabilities === undefined) return;
-                const capabilities: any = RTCRtpReceiver.getCapabilities("video");
-                if (p2pVp9On || p2pAv1On) {
-                    capabilities.codecs = sortCodecsByMimeType(capabilities.codecs, {
-                        vp9On: p2pVp9On,
-                        av1On: p2pAv1On,
-                    });
-                }
+            await Promise.all(
+                videoTransceivers.map(async (videoTransceiver) => {
+                    // If not implemented return
+                    if (RTCRtpReceiver.getCapabilities === undefined) return;
+                    if (videoTransceiver.setCodecPreferences === undefined) return;
 
-                // If not implemented return
-                if (videoTransceiver.setCodecPreferences === undefined) return;
-                videoTransceiver.setCodecPreferences(capabilities.codecs);
-            });
+                    const capabilities: any = RTCRtpReceiver.getCapabilities("video");
+                    if (p2pVp9On || p2pAv1On || preferHardwareDecodingOn) {
+                        capabilities.codecs = await sortCodecs(capabilities.codecs, {
+                            vp9On: p2pVp9On,
+                            av1On: p2pAv1On,
+                            preferHardwareDecodingOn,
+                        });
+                    }
+
+                    videoTransceiver.setCodecPreferences(capabilities.codecs);
+                }),
+            );
         } catch (error) {
             logger.error("Error during setting setCodecPreferences:", error);
         }
@@ -993,49 +989,48 @@ export default class P2pRtcManager implements RtcManager {
         }
         session.isOperationPending = true;
 
-        const { p2pVp9On, p2pAv1On, redOn, rtpAbsCaptureTimeOn, cleanSdpOn } = this._features;
+        const { p2pVp9On, redOn, rtpAbsCaptureTimeOn, cleanSdpOn } = this._features;
 
-        // Set codec preferences to video transceivers
-        if (p2pVp9On || p2pAv1On || redOn) {
-            this._setCodecPreferences(pc, { p2pVp9On, p2pAv1On, redOn });
-        }
-        pc.createOffer(constraints || this.offerOptions)
-            .then((offer: any) => {
-                // Add https://webrtc.googlesource.com/src/+/refs/heads/main/docs/native-code/rtp-hdrext/abs-capture-time
-                if (rtpAbsCaptureTimeOn) offer.sdp = addAbsCaptureTimeExtMap(offer.sdp);
-                // SDP munging workaround for Firefox, because it doesn't support setCodecPreferences()
-                // Only vp9 because FF does not support AV1 yet
-                if ((p2pVp9On || redOn) && browserName === "firefox") {
-                    offer.sdp = setCodecPreferenceSDP(offer.sdp, p2pVp9On, redOn);
-                }
+        this._setCodecPreferences(pc).then(() =>
+            pc
+                .createOffer(constraints || this.offerOptions)
+                .then((offer: any) => {
+                    // Add https://webrtc.googlesource.com/src/+/refs/heads/main/docs/native-code/rtp-hdrext/abs-capture-time
+                    if (rtpAbsCaptureTimeOn) offer.sdp = addAbsCaptureTimeExtMap(offer.sdp);
+                    // SDP munging workaround for Firefox, because it doesn't support setCodecPreferences()
+                    // Only vp9 because FF does not support AV1 yet
+                    if ((p2pVp9On || redOn) && browserName === "firefox") {
+                        offer.sdp = setCodecPreferenceSDP(offer.sdp, p2pVp9On, redOn);
+                    }
 
-                // workaround for two different browser bugs:
-                // chrome can modify existing transceiver(m section) adding duplicate payload types
-                // firefox seems to sometime break sdp (maybe duplicate payload types here as well)
-                //  when running setCodecPreferences on existing tranceivers.
-                // (preventing setCodecPreferences on existing tranceivers, limiting to new only
-                // , might be a better fix for firefox, but does not apply to chrome)
-                if (cleanSdpOn) offer.sdp = cleanSdp(offer.sdp);
+                    // workaround for two different browser bugs:
+                    // chrome can modify existing transceiver(m section) adding duplicate payload types
+                    // firefox seems to sometime break sdp (maybe duplicate payload types here as well)
+                    //  when running setCodecPreferences on existing tranceivers.
+                    // (preventing setCodecPreferences on existing tranceivers, limiting to new only
+                    // , might be a better fix for firefox, but does not apply to chrome)
+                    if (cleanSdpOn) offer.sdp = cleanSdp(offer.sdp);
 
-                pc.setLocalDescription(offer)
-                    .catch((e: any) => {
-                        logger.warn("RTCPeerConnection.setLocalDescription() failed with local offer", e);
+                    pc.setLocalDescription(offer)
+                        .catch((e: any) => {
+                            logger.warn("RTCPeerConnection.setLocalDescription() failed with local offer", e);
 
-                        // let app track this error
-                        this._emit(rtcManagerEvents.PC_SLD_FAILURE, e);
+                            // let app track this error
+                            this._emit(rtcManagerEvents.PC_SLD_FAILURE, e);
 
-                        throw e;
-                    })
-                    .then(() => {
-                        this._emitServerEvent(RELAY_MESSAGES.SDP_OFFER, {
-                            receiverId: clientId,
-                            message: this._transformOutgoingSdp(offer),
+                            throw e;
+                        })
+                        .then(() => {
+                            this._emitServerEvent(RELAY_MESSAGES.SDP_OFFER, {
+                                receiverId: clientId,
+                                message: this._transformOutgoingSdp(offer),
+                            });
                         });
-                    });
-            })
-            .catch((e: any) => {
-                logger.warn("RTCPeerConnection.createOffer() failed to create local offer", e);
-            });
+                })
+                .catch((e: any) => {
+                    logger.warn("RTCPeerConnection.createOffer() failed to create local offer", e);
+                }),
+        );
     }
 
     _withForcedRenegotiation(session: any, action: any) {
