02736699cc1e10563ff0f462aec842ee960ab578
Thomas Frivold <thomasdfrivold@gmail.com>
2025-07-02 09:46:40 +0200

    common: Add catalog version for jest

--- packages/react-native-sdk/jest.config.mjs
+++ packages/react-native-sdk/jest.config.mjs
@@ -1,4 +1,7 @@
+import path from "node:path";
 import buildConfig from "@whereby.com/jest-config/base";
+
+const __dirname = path.resolve();
 
 export default buildConfig(__dirname, {
     modulePathIgnorePatterns: ["<rootDir>/lib/"],
