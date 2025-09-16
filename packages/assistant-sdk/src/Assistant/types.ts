5e000ff9bef719242f942b4918c4afc743b58970
Thomas Frivold <thomasdfrivold@gmail.com>
2025-09-16 14:03:15 +0100

    common: Add events for assistant joining/leaving rooms

--- packages/assistant-sdk/src/Assistant/types.ts
+++ packages/assistant-sdk/src/Assistant/types.ts
@@ -1,5 +1,9 @@
 export const AUDIO_STREAM_READY = "AUDIO_STREAM_READY";
+export const ASSISTANT_JOINED_ROOM = "ASSISTANT_JOINED_ROOM";
+export const ASSISTANT_LEFT_ROOM = "ASSISTANT_LEFT_ROOM";
 
 export type AssistantEvents = {
+    [ASSISTANT_JOINED_ROOM]: [{ roomUrl: string }];
+    [ASSISTANT_LEFT_ROOM]: [{ roomUrl: string }];
     [AUDIO_STREAM_READY]: [{ stream: MediaStream; track: MediaStreamTrack }];
 };
