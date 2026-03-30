e1a623864ad8bf2ae07007c8e633af8bb32de5ea
Pontus Fagerström <pontus.fagerstrom@whereby.com>
2026-03-30 08:58:24 +0200

    media: change browser in test

--- packages/media/src/utils/__tests__/mediaSettings.spec.ts
+++ packages/media/src/utils/__tests__/mediaSettings.spec.ts
@@ -16,7 +16,7 @@ import { type Codec } from "../mediaSettings";
 import mockRouterRtpCapabilities from "./mockRouterRtpCapabilities.json";
 import assert from "assert";
 
-jest.mock("webrtc-adapter", () => ({ browserDetails: { browser: "firefox" } }));
+jest.mock("webrtc-adapter", () => ({ browserDetails: { browser: "chrome" } }));
 
 describe("sortCodecs", () => {
     const codecs: Codec[] = [
