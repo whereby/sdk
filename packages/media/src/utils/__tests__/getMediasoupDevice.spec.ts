06c721c1a86bf15f451070567e3016680c55bd78
Pontus Fagerström <144339208+pnts-se-whereby@users.noreply.github.com>
2026-04-28 06:27:20 +0200

    media: use mediasoup-client 3.19.0 (#989)

--- packages/media/src/utils/__tests__/getMediasoupDevice.spec.ts
+++ packages/media/src/utils/__tests__/getMediasoupDevice.spec.ts
@@ -1,10 +1,10 @@
 import { getMediasoupDeviceAsync } from "../getMediasoupDevice";
 
-jest.mock("@whereby.com/mediasoup-client", () => ({
+jest.mock("mediasoup-client", () => ({
     Device: jest.fn(),
     detectDeviceAsync: jest.fn(),
 }));
-const mediasoupClient = jest.requireMock("@whereby.com/mediasoup-client");
+const mediasoupClient = jest.requireMock("mediasoup-client");
 
 const safari17UserAgent =
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.4 Safari/605.1.15";
