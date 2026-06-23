6be9bc28cd7598caa2cf7d9dffcd7ad238a7a843
sarahrose-ferris <130575266+sarahrose-ferris@users.noreply.github.com>
2026-06-23 15:44:46 +0100

    core: decode room name on doAppStart

---
+++ .changeset/wise-socks-enter.md
@@ -0,0 +1,5 @@
+---
+"@whereby.com/core": minor
+---
+
+Decode roomUrl on doAppStart
--- packages/core/src/redux/slices/__tests__/app.unit.ts
+++ packages/core/src/redux/slices/__tests__/app.unit.ts
@@ -63,6 +63,88 @@ describe("appSlice", () => {
                     userAgent: "core:__PKG_VERSION__",
                 });
             });
+
+            it("should decode an encoded roomName", () => {
+                const initialConfig = {
+                    isNodeSdk: true,
+                    roomUrl: "https://some.url/r%C3%B6omname",
+                    roomKey: "roomKey",
+                    displayName: "displayName",
+                    externalId: "externalId",
+                };
+                const result = appSlice.reducer(undefined, appSlice.actions.doAppStart(initialConfig));
+
+                expect(result).toEqual({
+                    displayName: "displayName",
+                    externalId: "externalId",
+                    ignoreBreakoutGroups: false,
+                    initialConfig,
+                    isActive: true,
+                    isAssistant: false,
+                    isAudioRecorder: false,
+                    isDialIn: false,
+                    isNodeSdk: true,
+                    roomKey: "roomKey",
+                    roomName: "/röomname",
+                    roomUrl: "https://some.url/r%C3%B6omname",
+                    userAgent: "core:__PKG_VERSION__",
+                });
+            });
+
+            it("should leave an unencoded roomUrl unchanged", () => {
+                const initialConfig = {
+                    isNodeSdk: true,
+                    roomUrl: "https://some.url/roomname",
+                    roomKey: "roomKey",
+                    displayName: "displayName",
+                    externalId: "externalId",
+                };
+                const result = appSlice.reducer(undefined, appSlice.actions.doAppStart(initialConfig));
+
+                expect(result).toEqual({
+                    displayName: "displayName",
+                    externalId: "externalId",
+                    ignoreBreakoutGroups: false,
+                    initialConfig,
+                    isActive: true,
+                    isAssistant: false,
+                    isAudioRecorder: false,
+                    isDialIn: false,
+                    isNodeSdk: true,
+                    roomKey: "roomKey",
+                    roomName: "/roomname",
+                    roomUrl: "https://some.url/roomname",
+                    userAgent: "core:__PKG_VERSION__",
+                });
+            });
+
+            it("should return the original url if roomUrl contains a malformed URI", () => {
+                const initialConfig = {
+                    isNodeSdk: true,
+                    roomUrl: "https://some.url/%E0%A4%A",
+                    roomKey: "roomKey",
+                    displayName: "displayName",
+                    externalId: "externalId",
+                };
+
+                const result = appSlice.reducer(undefined, appSlice.actions.doAppStart(initialConfig));
+
+                expect(result).toEqual({
+                    displayName: "displayName",
+                    externalId: "externalId",
+                    ignoreBreakoutGroups: false,
+                    initialConfig,
+                    isActive: true,
+                    isAssistant: false,
+                    isAudioRecorder: false,
+                    isDialIn: false,
+                    isNodeSdk: true,
+                    roomKey: "roomKey",
+                    roomName: "/%E0%A4%A",
+                    roomUrl: "https://some.url/%E0%A4%A",
+                    userAgent: "core:__PKG_VERSION__",
+                });
+            });
         });
     });
 });
