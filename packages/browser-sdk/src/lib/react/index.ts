1ba3ce60bffbfa456f0bc5a7c8970a1cb1ac4fdf
Thomas Frivold <thomasdfrivold@gmail.com>
2026-08-21 16:04:01 +0100

    common: Add option to reply to a chat message with a file

--- packages/browser-sdk/src/lib/react/index.ts
+++ packages/browser-sdk/src/lib/react/index.ts
@@ -22,6 +22,7 @@ export type {
     ChatFileShare,
     FileUpload,
     FileShareError,
+    SendFilesOptions,
     CloudRecordingState as CloudRecording,
     LiveStreamState as LiveStreaming,
     BreakoutState as Breakout,
